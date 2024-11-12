04d38d289e21aaf4ca7c4e28a817d73a5d8ea33a
Flavio Silva Ferro <flavioferro@usp.br>
2024-11-12 18:53:55 -0300

    colocando static files padroes para ficar mais fácil

--- poliaero/settings.py
+++ poliaero/settings.py
@@ -124,11 +124,12 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
